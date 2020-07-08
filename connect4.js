3dff990eeed4e8db60285ae9947cbfed5bb05da8
Jedediah Arnold <jarnold910@gmail.com>
2020-07-08 15:22:48 -0400

    fix error of checking for rows that do not exist

--- connect4.js
+++ connect4.js
@@ -57,7 +57,7 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  for(let y = HEIGHT - 1; y <= HEIGHT; y--){
+  for(let y = HEIGHT - 1; y >= 0; y--){
     if(board[y][x] == null){
       return y;
     }
